a661d824ddda1cf304da2128c0b8cbb3080792ec
Yan <yan@metatem.net>
2025-07-17 16:29:30 +0200

    Converted to SDL3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This sample aims to reproduce inconsistencies between the state of the relative
-mouse mode and the cursor visibility on macOS with SDL 2.0.20.
+mouse mode and the cursor visibility on macOS, which was present in SDL 2.0.20.
 
 It opens a SDL window (with garbage content) and toggle relative mouse mode on
 pressing <kbd>Alt</kbd>.
@@ -9,7 +9,13 @@ When relative mouse mode is enabled, mouse events are logged to the console.
 To build and execute:
 
 ```bash
-meson x
+meson setup x
 ninja -Cx
 x/sample-relativemouse
 ```
+
+On macOS Catalina 10.15.7, with Command Line Tools 11.x installed, first do:
+```bash
+export SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk
+```
+or similar, depending on SDK version.
--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 
 int main() {
-    int r = SDL_Init(SDL_INIT_EVENTS);
-    if (r) {
+    bool r = SDL_Init(SDL_INIT_VIDEO);
+    if (!r) {
         fprintf(stderr, "Init failed: %s\n", SDL_GetError());
         exit(1);
     }
 
     atexit(SDL_Quit);
 
-    int x = SDL_WINDOWPOS_CENTERED;
-    int y = SDL_WINDOWPOS_CENTERED;
     int w = 256;
     int h = 256;
-    SDL_Window *window = SDL_CreateWindow("sample", x, y, w, h, 0);
-    if (!window) {
+    SDL_Window *window = SDL_CreateWindow("sample", w, h, SDL_WINDOW_ALWAYS_ON_TOP);
+    if (window == NULL) {
         fprintf(stderr, "Window creation failed: %s\n", SDL_GetError());
         exit(1);
     }
@@ -28,30 +26,30 @@ int main() {
     SDL_Event event;
     while (SDL_WaitEvent(&event)) {
         switch(event.type) {
-            case SDL_QUIT:
+            case SDL_EVENT_QUIT:
                 printf("Quit\n");
                 goto end;
-            case SDL_MOUSEMOTION:
-                if (SDL_GetRelativeMouseMode()) {
-                    printf("[relative mode] mouse motion (%d, %d)\n",
+            case SDL_EVENT_MOUSE_MOTION:
+                if (SDL_GetWindowRelativeMouseMode(window)) {
+                    printf("[relative mode] mouse motion (%f, %f)\n",
                            event.motion.xrel, event.motion.yrel);
                 }
                 break;
-            case SDL_MOUSEBUTTONUP:
-                if (SDL_GetRelativeMouseMode()) {
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+                if (SDL_GetWindowRelativeMouseMode(window)) {
                     printf("[relative mode] mouse button up\n");
                 }
                 break;
-            case SDL_MOUSEBUTTONDOWN:
-                if (SDL_GetRelativeMouseMode()) {
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                if (SDL_GetWindowRelativeMouseMode(window)) {
                     printf("[relative mode] mouse button down\n");
                 }
                 break;
-            case SDL_KEYDOWN:
+            case SDL_EVENT_KEY_DOWN:
                 // Toggle relative mode on Alt
-                if (event.key.keysym.sym == SDLK_LALT) {
-                    SDL_bool new_mode = !SDL_GetRelativeMouseMode();
-                    SDL_SetRelativeMouseMode(new_mode);
+                if (event.key.key == SDLK_LALT) {
+                    bool new_mode = !SDL_GetWindowRelativeMouseMode(window);
+                    SDL_SetWindowRelativeMouseMode(window, new_mode);
                     printf("switch relative mode to %s\n",
                            new_mode ? "true" : "false");
                 }
